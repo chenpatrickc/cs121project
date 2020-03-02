3d327b30d03d0c65d6424764e329d79b50e84dc3
Patrick Chen <patrickchen@wl-197-223.wpa.claremont.edu>
2020-03-02 00:20:23 -0800

    feat(filter): added filtering for sp500 companies

--- secScrape2.py
+++ secScrape2.py
@@ -2,7 +2,6 @@ import requests
 import urllib
 from bs4 import BeautifulSoup
 
-#make url
 def make_url(base_url, comp):
 
     url = base_url
@@ -17,34 +16,25 @@ def make_url(base_url, comp):
 base_url = r"https://www.sec.gov/Archives/edgar/daily-index"
 
 year = 2019
+
 # create the daily index url for 2019
 year_url = make_url(base_url, [year, 'index.json'])
-# print(year_url)
 
 # request the 2019 url
 content = requests.get(year_url)
 decoded_content = content.json()
-# print(decoded_content)
 
 filelist = []
+
 #loop through the dictionary
 for item in decoded_content['directory']['item']:
-
-    #get the name of the folder
-    # print('-'*100)
-    # print('Pulling url for quarter {}'.format(item['name']))
 
     #create the qtr url
     qtr_url = make_url(base_url, ['2019', item['name'], 'index.json'])
 
-    # print(qtr_url)
     #request the url and decode it
     file_content = requests.get(qtr_url)
     decoded_content = file_content.json()
-    # print(decoded_content)
-
-    # print('-'*100)
-    # print('Pulling files')
 
     for file in decoded_content['directory']['item']:
 
@@ -54,7 +44,16 @@ for item in decoded_content['directory']['item']:
 
 # file_url = r"https://www.sec.gov/Archives/edgar/daily-index/2019/QTR2/master.20190401.idx"
 
-for file_url in filelist[:31]:
+count = 0
+
+# make list of s&p 500 companies
+snp = []
+with open('S&PcompaniesCIK.txt') as f:
+    for line in f:
+        data = line.split()
+        snp.append(data[0])
+
+for file_url in filelist:
     # print(file_url)
     content = requests.get(file_url).content
 
@@ -64,25 +63,17 @@ for file_url in filelist[:31]:
     with open('master_20190401.txt', 'rb') as f:
         byte_data = f.read()
 
-    # print(byte_data)
     #decode the byte data
-    # data = byte_data.decode('utf-8').split('  ')
     data = byte_data.decode('utf-8').split('--------------------------------------------------------------------------------')
 
-    # print(data[1])
 
     # We need to remove the headers, so look for the end of the header and grab it's index
     for index, item in enumerate(data):
         if "ftp://ftp.sec.gov/edgar/" in item:
             start_ind = index + 1
-    # print('start index is ', start_ind)
 
     # define a new dataset with out the header info.
     data_format = data[start_ind:]
-
-    # data_format = data[1]
-    # print(data_format)
-    # print(len(data_format))
 
     master_data = []
 
@@ -101,7 +92,6 @@ for file_url in filelist[:31]:
                     mini_list[4] = "https://www.sec.gov/Archives/" + mini_list[4]
                     master_data.append(mini_list)
                     
-    # print(master_data[:3])
     # loop through each document in the master list.
     for index, document in enumerate(master_data):
         
@@ -115,38 +105,41 @@ for file_url in filelist[:31]:
         
         master_data[index] = document_dict
 
-    # print(master_data[:3])
-
 
     # by being in a dictionary format, it'll be easier to get the items we need.
     for document_dict in master_data:#[0:100]:
 
         # if it's a 10-K document pull the url and the name.
-        if document_dict['form_id'] == '10-K' or document_dict['form_id'] == 'NT 10-K':
-            # print(document_dict['file_url'])
-            # get the components
-            comp_name = document_dict['company_name']
-            docu_url = document_dict['file_url']
-            
-            print('-'*100)
-            print(comp_name)
-            print(docu_url)
+        if document_dict['cik_number'] in snp:
+            if document_dict['form_id'] == '10-K' or document_dict['form_id'] == 'NT 10-K':
+                count += 1
+                # get the components
+                comp_name = document_dict['company_name']
+                docu_url = document_dict['file_url']
+                
+                print('-'*100)
+                print(comp_name)
+                print(docu_url)
 
-            # Create a url that takes us to the Detail filing landing page
-            file_url_adj = docu_url.split('.txt')
-            file_url_archive = file_url_adj[0] + '-index.htm'
+                # Create a url that will take us to the archive folder
+                archive_url = docu_url.replace('.txt','').replace('-','')
 
-            print('-'*100)
-            print('     The Filing Detail can be found here: {}'.format(file_url_archive))
+                print('-'*100)
+                print('     The Archive Folder can be found here: {}'.format(archive_url))
 
-            # Create a url that will take us to the archive folder
-            archive_url = docu_url.replace('.txt','').replace('-','')
+                # # Create a url that takes us to the Detail filing landing page
+                # file_url_adj = docu_url.split('.txt')
+                # file_url_archive = file_url_adj[0] + '-index.htm'
 
-            print('-'*100)
-            print('     The Archive Folder can be found here: {}'.format(archive_url))
+                # print('-'*100)
+                # print('     The Filing Detail can be found here: {}'.format(file_url_archive))
 
-            # Create a url that will take us the Company filing Archive
-            company_url =docu_url.rpartition('/')
 
-            print('-'*100)
-            print('     The Company Archive Folder can be found here: {}'.format(company_url[0]))
+
+                # # Create a url that will take us the Company filing Archive
+                # company_url =docu_url.rpartition('/')
+
+                # print('-'*100)
+                # print('     The Company Archive Folder can be found here: {}'.format(company_url[0]))
+
+print("total count of companies is", count)
